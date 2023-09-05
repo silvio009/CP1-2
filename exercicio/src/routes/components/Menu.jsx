e1877f60450deb14600f250b0e191707754c7a7c
silvio009 <rm550821@fiap.com.br>
2023-09-05 16:50:05 -0300

    quarto commit

---
+++ exercicio/src/routes/components/Menu.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+
+export default function menu(){
+    return(
+        <nav className="menu">
+          <Link to ="/">Home</Link>
+        </nav>
+    )
+}
