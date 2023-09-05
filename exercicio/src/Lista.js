5fcfb7ff9b3c392f87db10571a2958640e8a798d
silvio009 <rm550821@fiap.com.br>
2023-09-05 19:26:02 -0300

    COMMIT

---
+++ exercicio/src/Lista.js
@@ -0,0 +1,17 @@
+export const listaProdutos = [
+    {
+        id : 1,
+        nome : "teclado",
+        preco : 150
+    },
+    {
+        id : 2,
+        nome : "mause",
+        preco : 150
+    },
+    {
+        id : 3,
+        nome : "monitor",
+        preco : 150
+    },
+]
--- exercicio/src/routes/Produtos/index.jsx
+++ exercicio/src/routes/Produtos/index.jsx
@@ -1,8 +1,18 @@
+import { listaProdutos } from "../../Lista";
+import { Link } from "react-router-dom";
+
 export default function Produtos(){
 
     return(
-        <div>
-            <h1>produtos</h1>
+        <main>
+        <h1>Produtos</h1>
+        {listaProdutos.map(prod => (
+        <div key={prod.id}>
+        <Link to={'/editar/produtos/${prod.id}'}>
+        editar o produto: {prod.nome}
+        </Link>
         </div>
+        ))}
+        </main>
     )
 }
