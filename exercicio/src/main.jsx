5abcd0342fe92016759960ef3d14cee599a65c77
silvio009 <rm550821@fiap.com.br>
2023-09-05 16:45:38 -0300

    terceiro commit

--- exercicio/src/main.jsx
+++ exercicio/src/main.jsx
@@ -8,3 +8,23 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './routers/Home.jsx';
+import Produtos from './routers/Produtos.jsx';
+import EditarProdutos from './routers/EditarProdutos.jsx';
+import Error from './routers/Error.jsx';
+
+const router = createBrowserRouter([
+  {path: "/" , element: <App/>, errorElement: <Error/>,
+    children:[
+      {path: "/" , element: <Home/>},
+      {path: "/produtos" , element: <Produtos/>},
+      {path: "/editar/produtos/:id" , element: <EditarProdutos/>},
+    ]}
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
