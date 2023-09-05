493c5da441cfd588538a60d72df9e9a4f41d2664
silvio009 <rm550821@fiap.com.br>
2023-09-05 18:59:12 -0300

    commit

--- exercicio/src/App.jsx
+++ exercicio/src/App.jsx
@@ -1,6 +1,10 @@
+
+import'./app.css'
 import { Outlet} from "react-router-dom";
 import Menu from "./routes/components/Menu";
 import Rodape from "./routes/components/Rodape";
+
+
 export default function App() {
   
 
