c4c226b0925147415024e974d2cb8669a6e293e4
silvio009 <rm550821@fiap.com.br>
2023-09-05 16:52:26 -0300

    sexto commit

--- exercicio/src/App.jsx
+++ exercicio/src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet} from "react-router-dom";
-
+import Menu from "./routes/components/Menu";
 export default function App() {
   
 
 
   return (
     <>
+        <Menu/>
         <Outlet/>
     </>
   )
